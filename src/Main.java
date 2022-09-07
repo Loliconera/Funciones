75ebf069a316a6931b4b3662ca2839556cdedcb2
Loliconera <69399372+Loliconera@users.noreply.github.com>
2022-09-07 01:14:46 -0500

    Funciones

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         int resultado;
         resultado = suma(50, 60, 80);
         System.out.println("El resultado de la suma es: "+ resultado);
-
         Coche miCoche = new Coche();
         miCoche.AñadirPuerta();
         miCoche.AñadirPuerta();
         miCoche.AñadirPuerta();
         miCoche.AñadirPuerta();
-        System.out.println("El numero de puertas es: " + miCoche.puertas);
+        System.out.println("El numero de puertas de mi coche es: " + miCoche.puertas);
     }
     public static int suma(int a, int b, int c) {
         return a + b + c;
     }
 }
-class Coche {
-    public int puertas = 0;
 
+    class Coche {
+
+    public int puertas = 0;
     public void AñadirPuerta() {
         this.puertas++;
     }
